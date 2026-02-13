47d6d5e81d4fc23a7cce9ee66cb8a7508489a4ba
nekomi <peytonporche46@gmail.com>
2026-02-12 22:05:36 -0500

    chore: update devDependencies and add ESLint configuration
    - Added eslint, globals, and typescript-eslint to devDependencies in package.json.
    - Created a new ESLint configuration file (eslint.config.mjs) with settings for JavaScript and TypeScript files.

--- commands/text/todo/viewTodo.js
+++ commands/text/todo/viewTodo.js
@@ -1,5 +1,11 @@
-const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, InteractionType } = require('discord.js');
-const { fetchUserTodoList } = require('./utils/fetchuserTodoList');
+const {
+    EmbedBuilder,
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    InteractionType,
+} = require("discord.js");
+const { fetchUserTodoList } = require("./utils/fetchuserTodoList");
 function paginate(text, itemsPerPage = 10) {
     const pages = [];
     for (let i = 0; i < text.length; i += itemsPerPage) {
@@ -9,51 +15,64 @@ function paginate(text, itemsPerPage = 10) {
 }
 function createEmbed(currentPage, items) {
     const embed = new EmbedBuilder()
-        .setTitle('Your Todo List')
-        .setDescription(items[currentPage].map((item, index) => `${currentPage * 10 + index + 1}. (${item.completed ? '✓' : '✗'}) ${item.content}`).join('\n'))
+        .setTitle("Your Todo List")
+        .setDescription(
+            items[currentPage]
+                .map(
+                    (item, index) =>
+                        `${currentPage * 10 + index + 1}. (${item.completed ? "✓" : "✗"}) ${item.content}`,
+                )
+                .join("\n"),
+        )
 
         .setFooter({ text: `Page ${currentPage + 1}/${items.length}` });
     return embed;
 }
 module.exports = {
-    name: 'view',
-    description: 'View your todo list',
-    aliases: ['vt', 'list'],
-    parent: 'todo',
+    name: "view",
+    description: "View your todo list",
+    aliases: ["vt", "list"],
+    parent: "todo",
     async execute(message, args) {
-
         const userId = message.author.id;
         const todos = await fetchUserTodoList(message.client, userId);
 
         if (todos.length === 0) {
-            return message.reply('Your todo list is empty!');
+            return message.reply("Your todo list is empty!");
         }
 
         const pages = paginate(todos, 10);
         let currentPage = 0;
         const embedMessage = await message.reply({
-            embeds: [createEmbed(currentPage, pages)], components: [
+            embeds: [createEmbed(currentPage, pages)],
+            components: [
                 new ActionRowBuilder().addComponents(
                     new ButtonBuilder()
-                        .setCustomId('prev')
-                        .setLabel('Previous')
+                        .setCustomId("prev")
+                        .setLabel("Previous")
                         .setStyle(ButtonStyle.Primary)
                         .setDisabled(currentPage === 0),
                     new ButtonBuilder()
-                        .setCustomId('next')
-                        .setLabel('Next')
+                        .setCustomId("next")
+                        .setLabel("Next")
                         .setStyle(ButtonStyle.Primary)
-                        .setDisabled(currentPage === pages.length - 1)
-                )
-            ]
+                        .setDisabled(currentPage === pages.length - 1),
+                ),
+            ],
         });
         const filter = (i) => i.user.id === message.author.id;
-        const collector = embedMessage.createMessageComponentCollector({ filter, idle: 60000 });
-        collector.on('collect', async (interaction) => {
+        const collector = embedMessage.createMessageComponentCollector({
+            filter,
+            idle: 60000,
+        });
+        collector.on("collect", async (interaction) => {
             if (interaction.type !== InteractionType.MessageComponent) return;
-            if (interaction.customId === 'next' && currentPage < pages.length - 1) {
+            if (
+                interaction.customId === "next" &&
+                currentPage < pages.length - 1
+            ) {
                 currentPage++;
-            } else if (interaction.customId === 'prev' && currentPage > 0) {
+            } else if (interaction.customId === "prev" && currentPage > 0) {
                 currentPage--;
             }
             await interaction.update({
@@ -61,22 +80,21 @@ module.exports = {
                 components: [
                     new ActionRowBuilder().addComponents(
                         new ButtonBuilder()
-                            .setCustomId('prev')
-                            .setLabel('Previous')
+                            .setCustomId("prev")
+                            .setLabel("Previous")
                             .setStyle(ButtonStyle.Primary)
                             .setDisabled(currentPage === 0),
                         new ButtonBuilder()
-                            .setCustomId('next')
-                            .setLabel('Next')
+                            .setCustomId("next")
+                            .setLabel("Next")
                             .setStyle(ButtonStyle.Primary)
-                            .setDisabled(currentPage === pages.length - 1)
-                    )
-                ]
+                            .setDisabled(currentPage === pages.length - 1),
+                    ),
+                ],
             });
         });
-        collector.on('end', async () => {
+        collector.on("end", async () => {
             await embedMessage.edit({ components: [] });
         });
-
-    }
+    },
 };
