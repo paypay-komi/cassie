bc49892502d2de591a3f4a179be3900a9c7163ee
nekomi <peytonporche46@gmail.com>
2026-02-12 21:54:59 -0500

    style: format sharder.js for consistent indentation and add .editorconfig for coding standards

--- sharder.js
+++ sharder.js
@@ -3,12 +3,12 @@ const path = require('path');
 const config = require('./config.json');
 
 const manager = new ShardingManager(path.join(__dirname, 'bot.js'), {
-  token: config.token,
-  totalShards: 'auto'
+    token: config.token,
+    totalShards: 'auto'
 });
 
 manager.on('shardCreate', shard => {
-  console.log('Launched shard ' + shard.id);
+    console.log('Launched shard ' + shard.id);
 });
 
 manager.spawn();
