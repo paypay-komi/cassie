2512048443ff49ad2bb9ead1b6d998626f35dcc5
nekomi <peytonporche46@gmail.com>
2026-02-17 19:06:10 -0500

    style: standardize quotation marks and formatting in reloadAllCommands.js

--- commands/slash/reloadAllCommands.js
+++ commands/slash/reloadAllCommands.js
@@ -1,15 +1,17 @@
 const reloadTextCommands = require("../../utils/reloadTextcommands");
 const reloadSlashcommands = require("../../utils/reloadSlashcommands");
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder } = require("@discordjs/builders");
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('reloadall')
-        .setDescription('Reloads all commands'),
-    async execute(interaction) {
-        await interaction.deferReply({ ephemeral: true });
+	data: new SlashCommandBuilder()
+		.setName("reloadall")
+		.setDescription("Reloads all commands"),
+	async execute(interaction) {
+		await interaction.deferReply({ ephemeral: true });
 
-         const textCommandsReloaded = reloadTextCommands(interaction.client);
-        const slashCommandsReloaded = reloadSlashcommands(interaction.client);
-        await interaction.editReply({ content: `✅ All commands reloaded! Text commands reloaded: ${textCommandsReloaded}, Slash commands reloaded: ${slashCommandsReloaded}` });
-    }
+		const textCommandsReloaded = reloadTextCommands(interaction.client);
+		const slashCommandsReloaded = reloadSlashcommands(interaction.client);
+		await interaction.editReply({
+			content: `✅ All commands reloaded! Text commands reloaded: ${textCommandsReloaded}, Slash commands reloaded: ${slashCommandsReloaded}`,
+		});
+	},
 };
