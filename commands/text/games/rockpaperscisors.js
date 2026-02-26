8e34aa10e5863019475e7c4d20d3714a2db4202d
nekomi <peytonporche46@gmail.com>
2026-02-25 23:23:31 -0500

    more work done on tic tac toe it now sends the starting game message for ai mode

--- commands/text/games/rockpaperscisors.js
+++ commands/text/games/rockpaperscisors.js
@@ -1,10 +1,39 @@
-const { ButtonBuilder } = require("discord.js");
+const {
+	ButtonBuilder,
+	ButtonStyle,
+	ActionRowBuilder,
+	Component,
+} = require("discord.js");
 module.exports = {
 	name: "rockPaperScissors",
 	parent: "games",
 	aliases: ["rps"],
-	execute(message, args) {
-		if (message.mentions.first()) {
+	async execute(message, args) {
+		const game_message = await message.reply("loading...");
+		function makeButtons(user) {
+			function makeButton(emoji) {
+				return new ButtonBuilder()
+					.setStyle(ButtonStyle.Primary)
+					.setCustomId(`rps-${message.id}-${user.id}-${emoji}`)
+					.setLabel(emoji);
+			}
+			const buttons = [];
+			buttons.push(makeButton("🪨"));
+			buttons.push(makeButton("📄"));
+			buttons.push(makeButton("✂️"));
+			return new ActionRowBuilder().addComponents(buttons);
+		}
+		let is_ai = true;
+		let other_player = await message.client.user.fetch();
+		if (message.mentions.users.first()) {
+			other_player = message.mentions.users.first();
+			is_ai = false;
+		}
+		if (is_ai) {
+			await game_message.edit({
+				components: [makeButtons(message.member.user)],
+				content: "pick your weapon",
+			});
 		}
 		message.reply("this is wip");
 	},
