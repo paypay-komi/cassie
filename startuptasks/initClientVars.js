8ee12545474d9f063bb4c1b7f9f827864d3d75d3
nekomi <peytonporche46@gmail.com>
2026-03-12 18:15:08 -0400

    same as prev commit

--- startuptasks/initClientVars.js
+++ startuptasks/initClientVars.js
@@ -1,8 +1,8 @@
-const { Collection } = require('discord.js');
-const config = require('../config.json');
+const { Collection } = require("discord.js");
+const config = require("../config.json");
 module.exports = {
-	name: 'initClientVars',
-	description: 'Initialize client variables on startup',
+	name: "initClientVars",
+	description: "Initialize client variables on startup",
 	reloadAble: true, // some times we might want to reset these without restarting the bot
 	priority: 1000000000,
 	execute(client) {
@@ -12,7 +12,8 @@ module.exports = {
 		client.subcommandMap = {}; // parent → { subName → command }
 		client.commandSettings = {}; // future per-channel/role/user overrides
 		client.prefix = config.prefix;
-		client.afk = new Map()
-		console.log('✅ Client variables initialized on startup');
+		client.afk = new Map();
+		client.__eventListeners = [];
+		console.log("✅ Client variables initialized on startup");
 	},
 };
