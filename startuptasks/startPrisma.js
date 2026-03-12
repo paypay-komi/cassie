4781bd3364d5e4548ecbfd0a1b84db4ae8d18c5c
nekomi <peytonporche46@gmail.com>
2026-03-12 16:32:43 -0400

    addeda a priorty to start prisma

--- startuptasks/startPrisma.js
+++ startuptasks/startPrisma.js
@@ -1,7 +1,10 @@
+const { priority } = require('./initClientVars.js');
+
 module.exports = {
 	name: 'startPrisma',
 	description: 'Start Prisma client on startup',
 	reloadAble: false,
+	priority: 10000000000,
 	execute(client) {
 		client.db = require('../db/boobs.js'); // Prisma client instance
 		console.log('✅ Prisma client started on startup');
