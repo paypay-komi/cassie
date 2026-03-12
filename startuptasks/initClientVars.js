fd6a80bb9ea702e417df63ac572cca11f8ef07d1
nekomi <peytonporche46@gmail.com>
2026-03-12 16:33:19 -0400

    added a priorty to initcale the client vars

--- startuptasks/initClientVars.js
+++ startuptasks/initClientVars.js
@@ -4,6 +4,7 @@ module.exports = {
 	name: 'initClientVars',
 	description: 'Initialize client variables on startup',
 	reloadAble: true, // some times we might want to reset these without restarting the bot
+	priority: 1000000000,
 	execute(client) {
 		client.owners = config.owners || [];
 		client.slashCommands = new Collection();
@@ -11,6 +12,7 @@ module.exports = {
 		client.subcommandMap = {}; // parent → { subName → command }
 		client.commandSettings = {}; // future per-channel/role/user overrides
 		client.prefix = config.prefix;
+		client.afk = new Map()
 		console.log('✅ Client variables initialized on startup');
 	},
 };
