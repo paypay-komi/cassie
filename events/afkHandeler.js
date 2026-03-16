a497ba874d3937ec028c770db6c47949e103a0a6
nekomi <peytonporche46@gmail.com>
2026-03-16 15:32:29 -0400

    feat(afk): replace raw AFK date with formatted Discord timestamps

--- events/afkHandeler.js
+++ events/afkHandeler.js
@@ -1,5 +1,6 @@
 const { JsonNull } = require("@prisma/client/runtime/client");
 const { Events } = require("discord.js");
+const { time } = require("discord.js");
 
 module.exports = {
 	name: Events.MessageCreate,
@@ -12,7 +13,9 @@ module.exports = {
 		for (const user of mentions.users.values()) {
 			const afk_data = client.afk.get(user.id);
 			if (!afk_data) continue;
-			final_message += `<@${afk_data.userId}> has been afk since: ${afk_data.since}\n reason: ${afk_data.reason}\n`;
+			const date = new Date(afk_data.since);
+
+			final_message += `<@${afk_data.userId}> has been afk since: ${time(date)} (${time(date, "R")})\n reason: ${afk_data.reason}\n`;
 		}
 		if (final_message == "") return;
 		message.reply(final_message);
