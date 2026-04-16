2593f2f5e71f05b491cc803c299d910ed71b50f5
nekomi <peytonporche46@gmail.com>
2026-04-16 18:02:21 -0400

    feat(time): add initial implementation of get command for user time retrieval

---
+++ commands/text/time/get.js
@@ -0,0 +1,11 @@
+const { description } = require("./set");
+
+module.exports = {
+	name: "get",
+	parent: "time",
+	description: "gets the time from a mentioned user",
+	execute(message, args) {
+		message.reply("this is a wip dont wanna work on this right now lol")
+
+	}
+}
