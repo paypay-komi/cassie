02a0f26120bb0f5100a3c10e0d8f7b883d18ea26
nekomi <peytonporche46@gmail.com>
2026-04-19 14:22:46 -0400

    finished the timezone getting

--- commands/text/time/get.js
+++ commands/text/time/get.js
@@ -1,6 +1,6 @@
-const { description } = require("./set");
 const { Message } = require("discord.js");
 const db = require("../../../db/boobs.js");
+const { DateTime } = require("luxon");
 const { time } = require("node:console");
 module.exports = {
 	name: "get",
@@ -9,7 +9,7 @@ module.exports = {
 	/**
 	 *
 	 * @param {Message} message
-	 * @param {*} args
+	 * @param {String[]} args
 	 */
 	async execute(message, args) {
 		if (!message.mentions.users.first()) {
@@ -17,13 +17,22 @@ module.exports = {
 				"you must mention a user to get their time silly ",
 			);
 		}
-		const time_zone_data = await db.prisma.userTimezone.findUnique({
-			where: { userId: message.mentions.users.first().id },
-		});
-		if (!time_zone_data)
+		const other_persons_time_zone_data =
+			await db.prisma.userTimezone.findUnique({
+				where: { userId: message.mentions.users.first().id },
+			});
+		if (!other_persons_time_zone_data)
 			return message.reply(
-				"this user has not set their timezone ask them to run c.time set",
+				`${message.mentions.users.first().displayName} has not set their timezone ask them to run c.time set`,
 			);
-		message.reply(JSON.stringify(time_zone_data, null, 2));
+		let timezone;
+		if (other_persons_time_zone_data.timeZoneString)
+			timezone = other_persons_time_zone_data.timeZoneString;
+		else
+			timezone = `UTC${Math.round(other_persons_time_zone_data.minsOffset / 60)}`;
+		const dt = DateTime.now().setZone(timezone);
+		message.reply(
+			`other persons time: \n ${dt.toFormat("hh:mm")}\n dst: ${dt.isInDST}`,
+		);
 	},
 };
