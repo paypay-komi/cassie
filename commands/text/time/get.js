593943c7378c2f8a37fe6d825e6d3a36a9cabbb3
nekomi <peytonporche46@gmail.com>
2026-04-19 13:14:52 -0400

    feat(time): implement user time retrieval in get command

--- commands/text/time/get.js
+++ commands/text/time/get.js
@@ -1,11 +1,29 @@
 const { description } = require("./set");
-
+const { Message } = require("discord.js");
+const db = require("../../../db/boobs.js");
+const { time } = require("node:console");
 module.exports = {
 	name: "get",
 	parent: "time",
 	description: "gets the time from a mentioned user",
-	execute(message, args) {
-		message.reply("this is a wip dont wanna work on this right now lol")
-
-	}
-}
+	/**
+	 *
+	 * @param {Message} message
+	 * @param {*} args
+	 */
+	async execute(message, args) {
+		if (!message.mentions.users.first()) {
+			return message.reply(
+				"you must mention a user to get their time silly ",
+			);
+		}
+		const time_zone_data = await db.prisma.userTimezone.findUnique({
+			where: { userId: message.mentions.users.first().id },
+		});
+		if (!time_zone_data)
+			return message.reply(
+				"this user has not set their timezone ask them to run c.time set",
+			);
+		message.reply(JSON.stringify(time_zone_data, null, 2));
+	},
+};
