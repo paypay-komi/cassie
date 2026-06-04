8951383320c3c413142b4fbdfd3e82e0a21f2df8
nekomi <peytonporche46@gmail.com>
2026-06-03 21:58:08 -0400

    refactor: remove Connect4Game test and router files

--- commands/text/games/connect4/router.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    name: "connect4",
-    description: "Play Connect 4 against another user or the AI",
-    parent: "games",
-    aliases: ["connectfour", "c4", "connect-four"],
-};
