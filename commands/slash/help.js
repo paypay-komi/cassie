a127154c806e58337fe1f9dde86b71b7909188ad
nekomi <peytonporche46@gmail.com>
2026-06-04 00:37:14 -0400

    feat: add help slash command to provide usage instructions

---
+++ commands/slash/help.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("help")
+		.setDescription("Learn how to use this bot"),
+	async execute(interaction) {
+		const prefix = interaction.client.prefix ?? "c.";
+		await interaction.reply({
+			content:
+				`This bot uses text commands, not slash commands. Use \`${prefix}help\` to see all commands.\n\n` +
+				`Example: \`${prefix}8ball will i win?\``,
+			ephemeral: true,
+		});
+	},
+};
